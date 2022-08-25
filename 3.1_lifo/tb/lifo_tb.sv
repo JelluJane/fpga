19e5766dfe7762d04f22ba05eb614cda4890f874
JelluJane <jub1lee@yandex.ru>
2022-08-25 12:55:46 +0300

    25.08.22

--- 3.1_lifo/tb/lifo_tb.sv
+++ 3.1_lifo/tb/lifo_tb.sv
@@ -56,23 +56,23 @@ lifo #(
 
 task wr_only();
   ##1;
-  wrreq <= 1'b1;
-  rdreq <= 1'b0;
-  data <= $urandom();
+  wrreq = 1'b1;
+  rdreq = 1'b0;
+  data = $urandom();
   read_data_ref.push_front( data );
 endtask
 
 task rd_only();
-  rdreq <= 1'b1;
-  wrreq <= 1'b0;
+  rdreq = 1'b1;
+  wrreq = 1'b0;
   ##1;
   read_data_dut.push_back( q );
 endtask
 
 task idle();
   ##1;
-  rdreq <= 1'b0;
-  wrreq <= 1'b0;
+  rdreq = 1'b0;
+  wrreq = 1'b0;
   ##1;
 endtask 
 
@@ -83,6 +83,7 @@ task check_data ();
         begin
           tmp_dut = read_data_dut.pop_back();
           tmp_ref = read_data_ref.pop_back();
+          $display("dut is %d ref is %d", tmp_dut, tmp_ref);
           if ( tmp_dut != tmp_ref )
             $error( "the data does not match" );
         end
@@ -91,6 +92,7 @@ endtask
 task testcase0();
   read_data_dut = {};
   read_data_ref = {};
+  test_len = ( 2**AWIDTH ) + 1;
   $display( "Testcase0: write until full, then read until empty.");
   repeat( ( 2**AWIDTH ) + 1 ) wr_only();
   repeat( ( 2**AWIDTH ) + 1 ) rd_only();
@@ -111,6 +113,7 @@ endtask
 task testcase2();
   read_data_dut = {};
   read_data_ref = {};
+  test_len = ( 2**AWIDTH ) + 1;
   $display( "Testcase2: write until overflow, then read until empty.");
   repeat( ( 2**AWIDTH ) + 2 ) wr_only();
   if ( usedw > 2**AWIDTH )
